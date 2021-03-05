46679b80afa82236531f85e6079a1bfa7188fe72
g3john <lacunajo@gmail.com>
2021-03-04 19:25:07 -0500

    Fix saved page when empty

--- components/savedPage/component.tsx
+++ components/savedPage/component.tsx
@@ -2,25 +2,34 @@ import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import JobList from '../jobList';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 const SavedPage = (props) => {
   const { jobs, navigation } = props;
+  const hasSavedJobs = jobs.length > 0;
+  const containerStyle = hasSavedJobs ? {} : styles.container;
   return (
-    <View>
+    <View style={containerStyle}>
       {jobs.length > 0 ? (
         <View>
           <JobList ref={null} jobs={jobs} navigation={navigation} />
         </View>
       ) : (
-        <View>
-          <Ionicons name={'star'} size={40} color={'black'} />
-          <Text>No favourites!</Text>
+        <View style={styles.container}>
+          <Ionicons name={'star-outline'} size={40} color={Colors.primary} />
+          <Text>No saved jobs</Text>
         </View>
       )}
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default SavedPage;
